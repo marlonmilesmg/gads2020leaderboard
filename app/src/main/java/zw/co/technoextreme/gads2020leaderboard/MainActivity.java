2f0787a4d73d07cbe97f143c358da897f237a09e
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2020-09-07 10:20:03 +0200

    Inception

---
+++ app/src/main/java/zw/co/technoextreme/gads2020leaderboard/MainActivity.java
@@ -0,0 +1,14 @@
+package zw.co.technoextreme.gads2020leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
