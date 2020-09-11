48a93a315ab89df743de153c5033c983a305b567
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2020-09-12 00:36:19 +0200

    Layouts

--- app/src/main/java/zw/co/technoextreme/gads2020leaderboard/LearningLeadersFragment.java
+++ app/src/main/java/zw/co/technoextreme/gads2020leaderboard/LearningLeadersFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class LearningLeadersFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -20,15 +20,5 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.learning_leaders_fragment, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-    }
 }
--- app/src/main/java/zw/co/technoextreme/gads2020leaderboard/SkilliqLeadersFragment.java
+++ app/src/main/java/zw/co/technoextreme/gads2020leaderboard/SkilliqLeadersFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class SkilliqLeadersFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -20,15 +20,5 @@ public class SecondFragment extends Fragment {
         return inflater.inflate(R.layout.skilliq_leaders_fragment, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
-    }
 }
